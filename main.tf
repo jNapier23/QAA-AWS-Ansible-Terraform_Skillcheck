ee14bf2a70a6b6a24bc2d4d657bed10032d25455
James_Mac <james_napier@outlook.com>
2023-04-03 12:21:28 +0100

    Removed upgrade command

--- main.tf
+++ main.tf
@@ -176,7 +176,6 @@ resource "aws_instance" "pipeline" {
         //installs jenkins
         "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
         "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
-        "sudo apt upgrade -y",
         "sudo apt-get update -y",
         "sudo apt-get install jenkins -y"
     ]
