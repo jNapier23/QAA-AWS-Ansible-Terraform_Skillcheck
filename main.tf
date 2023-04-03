498ae77ab3648896bd2fd311d683a8b3f53a5ee3
James_Mac <james_napier@outlook.com>
2023-04-03 12:33:42 +0100

    Running out of ideas to fix issue, installing curl

--- main.tf
+++ main.tf
@@ -173,11 +173,12 @@ resource "aws_instance" "pipeline" {
         //installs java
         "sudo apt update",
         "sudo apt install openjdk-11-jdk -y",
+        "sudo apt install curl -y",
         //installs jenkins
         "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
         "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
-        "sudo apt-get update",
-        "sudo apt-get install jenkins"
+        "sudo apt-get update -y && sudo apt-get upgrade -y",
+        "sudo apt-get install jenkins -y"
     ]
 
 }
