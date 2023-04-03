0c2a5c6ad4ae7dfd9be00d782a3dd4c587e78840
James_Mac <james_napier@outlook.com>
2023-04-03 12:05:22 +0100

    added upgrade command

--- main.tf
+++ main.tf
@@ -176,6 +176,7 @@ resource "aws_instance" "pipeline" {
         //installs jenkins
         "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
         "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
+        "sudo apt upgrade -y",
         "sudo apt-get update -y",
         "sudo apt-get install jenkins -y"
     ]
