5ad31df1907c31e0b41fd24cc04bee45209cf9d8
James_Mac <james_napier@outlook.com>
2023-04-04 16:00:37 +0100

    Added missing [] to new cidr block in http rule

--- main.tf
+++ main.tf
@@ -142,7 +142,7 @@ resource "aws_security_group" "project_sg" {
         from_port           = 80
         to_port             = 80
         protocol            = "tcp"
-        cidr_blocks         = "0.0.0.0/0"
+        cidr_blocks         = ["0.0.0.0/0"]
     }
     
     egress {
