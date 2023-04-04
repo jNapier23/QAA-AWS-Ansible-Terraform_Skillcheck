08a8b23128116e73934f31b9de05a1f9e1905121
James_Mac <james_napier@outlook.com>
2023-04-04 12:31:15 +0100

    Added commands to install jenkins on new instance

--- main.tf
+++ main.tf
@@ -175,20 +175,20 @@ resource "aws_instance" "pipeline" {
         private_key         = tls_private_key.private_key.private_key_pem
     }
 
-    # provisioner "remote-exec" {
-    # inline = [
-    #     //installs java
-    #     "sudo apt update",
-    #     "sudo apt install openjdk-11-jdk -y",
-    #     //installs jenkins
-    #     "sudo apt install ca-certificates",
-    #     "curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null",
-    #     "echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null",
-    #     "sudo apt-get update",
-    #     "sudo apt-get install jenkins"
-    # ]
+    provisioner "remote-exec" {
+    inline = [
+        //installs java
+        "sudo yum update -y",
+        "sudo amazon-linux-extras install java-openjdk11 -y",
+        //installs jenkins
+        "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
+        "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
+        "sudo yum upgrade",
+        "sudo yum install jenkins -y",
+        "sudo systemctl start jenkins"
+    ]
 
-    #}
+    }
     
     tags = {
         Name                = "Pipeline Instance"
