c8a8ebc06669c4823a5831153da186777b113ad0
James_Mac <james_napier@outlook.com>
2023-04-13 16:01:40 +0100

    Moved non-working sh cmds from ansible to .sh file

--- install-app.sh
+++ install-app.sh
@@ -2,10 +2,16 @@
 
 # Automates install of app dependencies, clones app to instance, and runs app
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.37.2/install.sh | bash 
+. ~/.nvm/nvm.sh
+nvm install 16 
+
 npm install -g express-generator 
 express
 npm install
+
 sudo yum install git
 git clone https://gitlab.com/Reece-Elder/devops-m5-nodeproject/
 cd devops-m5-nodeproject
+
 node index.js 
